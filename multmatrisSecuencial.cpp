959e4768cfaf3519ed45ae6187f1797321871de2
karen-lopez <stefanny@utp.edu.co>
2019-09-06 10:08:56 -0500

    secuencial

---
+++ multmatrisSecuencial.cpp
@@ -0,0 +1,48 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+//funcion que multiplica 2 matrices (A Y B) de tamaño NXN y
+//retorna el resultado de la multiplicacion
+vector<vector< int > > multmatris(vector<vector< int > > A, vector<vector< int > > B, N){
+  vector<vector< int > > C(N,vector<int>(N));
+
+  for(int i= 0; i<N; ++i){
+        for(int j=0; j<N; ++j){
+            for(int z=0; z<N; ++z){
+                C[i][j] += A[i][z] * B[z][j];
+            }
+          }
+  }
+  return C;
+}
+
+int main(){
+  
+    int N;
+    scanf("%d", &N);
+    vector<vector< int > > A(N,vector<int>(N));
+    vector<vector< int > > B(N,vector<int>(N));
+    vector<vector< int > > C(N,vector<int>(N));
+    clock_t times;
+
+
+    //rellenar matrices
+    for(int i=0; i<N; i++){
+
+             for(int j=0; j<N; j++){
+
+                    A[i][j] = rand() % 10;
+                    B[i][j] = rand() % 10;
+
+              }
+
+    }
+
+    times = clock();
+    C = multmatris(A, B, N);
+    times = clock() - times;
+
+    printf("%f\n", ((float)times)/CLOCKS_PER_SEC);
+
+}
